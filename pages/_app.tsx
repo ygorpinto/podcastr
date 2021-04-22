97725a0b6914127bb88dc2d4e7bb0515892a4400
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-04-22 14:11:37 -0300

    ok

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "../styles/global"
+
+function MyApp({ Component, pageProps }) {
+  return (
+      <>
+        <GlobalStyles/>
+        <Component {...pageProps} />
+      </>
+  )
+}
+
+export default MyApp
