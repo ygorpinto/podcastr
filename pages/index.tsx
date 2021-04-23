1c2439ff68f535fbb728b2d8b31d6f030af1ae10
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-04-23 10:45:30 -0300

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
-export default function Home() {
+export default function Home({episodes}) {
   return (
-    <div>
-    </div>
+    <>
+    <h1>Episodes</h1>
+    {JSON.stringify(episodes)}
+    </>
   )
+}
+
+export async function getStaticProps () {
+  const res = await fetch ("http://localhost:3333/episodes");
+  const data = await res.json();
+
+  return {
+    props: {
+      episodes:data
+    },
+    revalidate: 60 * 60 * 4
+  }
 }
