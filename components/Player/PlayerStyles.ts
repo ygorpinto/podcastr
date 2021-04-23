443f325ef14f8d36008d47fb00f7c52d25d18d7a
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-04-23 09:24:41 -0300

    ok

---
+++ components/Player/PlayerStyles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+const PlayerStyles = styled.div`
+
+`
+export default PlayerStyles;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+import Header from "../components/Header/Header"
+import Player from "../components/Player/Player"
 import GlobalStyles from "../styles/global"
 
 function MyApp({ Component, pageProps }) {
   return (
       <>
         <GlobalStyles/>
+        <Header/>
+        <Player/>
         <Component {...pageProps} />
       </>
   )
